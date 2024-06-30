8730acb5138e399fa26b384ee0fccbb109e19ef5
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-06-30 07:05:35 +0530

    Added night weather icons and set the code to change background according to the day and night

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const searchBox = document.querySelector(".search input");
 const searchbtn = document.querySelector(".search button");
 const weather_icon = document.querySelector(".weather-icon");
 
+
 async function getWeather(city){
     const resp = await fetch(apiUrl+city+`&appid=${apiKey}`); 
 
@@ -14,31 +15,78 @@ async function getWeather(city){
     }else{
         const data = await resp.json();
         console.log(data);
-
+        
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML =Math.round(data.main.temp) + "°C";
         document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
         document.querySelector(".wind").innerHTML=data.wind.speed +"km/h";
 
-        if(data.weather[0].main=="Clouds"){
-            weather_icon.src="images/clouds.png";
-        }else if(data.weather[0].main=="Clear"){
-            weather_icon.src="images/clear.png";
-        }else if(data.weather[0].main=="Rain"){
-            weather_icon.src="images/rain.png";
-        }else if(data.weather[0].main=="Drizzle"){
-            weather_icon.src="images/drizzle.png";
-        }else if(data.weather[0].main=="Mist"){
-            weather_icon.src="images/mist.png"; 
-        }else{
-            weather_icon.src="images/snow.png";
-        }
+        currentWeatherData = data;
+        updateWeathericon(data);
 
         document.querySelector(".weather").style.display="block";
         document.querySelector(".error").style.display="none";
     }
  
 }
+
+function updateWeathericon(data){
+    const currentHour = new Date().getHours();
+    if(data.weather[0].main=="Clouds"){
+        if (currentHour >= 6 && currentHour < 18) {
+            weather_icon.src="images/day-clouds.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #00c6ff, #0072ff)";
+        }else{
+            weather_icon.src="images/night-clouds.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #0f0c29, #302b63, #24243e)";
+        }
+    }else if(data.weather[0].main=="Clear"){
+        if (currentHour >= 6 && currentHour < 18) {
+            weather_icon.src="images/day-clear.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #00c6ff, #0072ff)";
+        }else{
+            weather_icon.src="images/night-clear.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #0f0c29, #302b63, #24243e)";
+        }
+    }else if(data.weather[0].main=="Rain"){
+        if (currentHour >= 6 && currentHour < 18) {
+            weather_icon.src="images/day-rain.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #00c6ff, #0072ff)";
+        }else{
+            weather_icon.src="images/night-rain.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #0f0c29, #302b63, #24243e)";
+        }
+    }else if(data.weather[0].main=="Drizzle"){
+        if (currentHour >= 6 && currentHour < 18) {
+            weather_icon.src="images/day-drizzle.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #00c6ff, #0072ff)";
+        }else{
+            weather_icon.src="images/night-drizzle.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #0f0c29, #302b63, #24243e)";
+        }
+    }else if(data.weather[0].main=="Mist"){
+        if (currentHour >= 6 && currentHour < 18) {
+            weather_icon.src="images/day-mist.png"; 
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #00c6ff, #0072ff)";
+        }else{
+            weather_icon.src="images/night-mist.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #0f0c29, #302b63, #24243e)";
+        }
+    }else if(data.weather[0].main=="Snow"){
+        if (currentHour >= 6 && currentHour < 18) {
+            weather_icon.src="images/day-snow.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #00c6ff, #0072ff)";
+        }else{
+            weather_icon.src="images/night-snow.png";
+            document.querySelector(".card").style.background="linear-gradient(to bottom, #0f0c29, #302b63, #24243e)";
+        }
+    }
+};
+setInterval(() => {
+    if (currentWeatherData) {
+        updateWeatherIconAndBackground(currentWeatherData);
+    }
+}, 60000);
 
 searchbtn.addEventListener("click", ()=>{
     getWeather(searchBox.value);
@@ -52,6 +100,17 @@ getWeather(initialLocation);
 setInterval(() => {
     const currentLocation = searchBox.value;
     if (currentLocation) {
-        getWeatherWeather(currentLocation);
+        getWeather(currentLocation);
     }
-},6000); // 1 minute
+},60000); // 1 minute
+
+
+
+// get time & date
+setInterval(() => {
+    let d = new Date();
+    let time = d.toLocaleTimeString();
+    let date = d.toLocaleDateString();
+    document.querySelector(".Time").innerHTML = time;
+    document.querySelector(".Date").innerHTML = date;
+},1000)
