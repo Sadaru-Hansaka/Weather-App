2d823a667deb2f4d26f15ef00ac1a765c0de9c89
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-07-11 15:27:45 +0530

    Developed the application to assign weather image according to the weather data and changed some layouts and bg color

--- template.js
+++ template.js
@@ -1,10 +1,11 @@
 // template.js
 export class Template {
-    constructor(cityName, country,temp,cityArray) {
+    constructor(cityName, country,temp,cityArray,weather) {
         this.cityName = cityName;
         this.country = country;
         this.temp = temp;
         this.cityArray = cityArray;
+        this.weather = weather;
     }
 
     createBox() {
@@ -14,7 +15,6 @@ export class Template {
         const closeIcon = document.createElement('span');
         closeIcon.innerHTML = '&times;'; // HTML entity for '×'
         closeIcon.classList.add('close_icon');
-        closeIcon.style.cursor = 'pointer'; // Make the icon look clickable
 
         closeIcon.addEventListener('click', () => {
             
@@ -31,9 +31,60 @@ export class Template {
 
         boxElement.appendChild(closeIcon);
 
-        const textElement = document.createElement('p');
-        textElement.innerText = `Name: ${this.cityName}\nCountry: ${this.country}\nTemperature: ${this.temp}`;
-        boxElement.appendChild(textElement);
+        const template_container = document.createElement('div');
+        template_container.classList.add('template_container');
+        
+        
+        const imageElement = document.createElement('div');
+        imageElement.classList.add('template-image');
+        const weath = this.weather;
+        const weahterImg = document.createElement('img');
+        weahterImg.classList.add('template_img');
+        
+        switch(weath){
+            case "Clouds":
+                weahterImg.src = '../images/day-clouds.png';
+                weahterImg.alt = 'Clouds';
+                imageElement.appendChild(weahterImg);
+                break;
+            case "Rain":
+                weahterImg.src = '../images/day-rain.png';
+                weahterImg.alt = 'Rain';
+                imageElement.appendChild(weahterImg);
+                break;
+            case "Clear":
+                weahterImg.src = '../images/day-clear.png';
+                weahterImg.alt = 'Clear';
+                imageElement.appendChild(weahterImg);
+                break;
+            case "Snow":
+                weahterImg.src = '../images/day-snow.png';
+                weahterImg.alt = 'Snow';
+                imageElement.appendChild(weahterImg);
+                break;
+            case "Drizzle":
+                weahterImg.src = '../images/day-drizzle.png';
+                weahterImg.alt = 'Drizzle';
+                imageElement.appendChild(weahterImg);
+                break;
+            case "Mist":
+                weahterImg.src = '../images/day-mist.png';
+                weahterImg.alt = 'mist';
+                imageElement.appendChild(weahterImg);
+                break;
+            default:
+                imageElement.innerText=`Name: ${this.weather}`;
+        }
+        
+
+        template_container.appendChild(imageElement);
+
+        const textElement = document.createElement('div');
+        textElement.classList.add('template-text');
+        textElement.innerText = `Name: ${this.cityName}\nCountry: ${this.country}\nTemperature: ${this.temp}\nWeather: ${this.weather}`;
+        template_container.appendChild(textElement);
+
+        boxElement.appendChild(template_container);
         
         return boxElement;
     }    
@@ -61,7 +112,8 @@ export function handleWeathercard(plusIcon, searchBox,cityArray,getWeatherByCity
                     const cityName = item.data.name; // Access city name correctly
                     const couuntry = item.data.sys.country;
                     const temp = item.data.main.temp;
-                    const template = new Template(cityName,couuntry, temp,cityArray);
+                    const weather = item.data.weather[0].main;
+                    const template = new Template(cityName,couuntry, temp,cityArray,weather);
                     const newBox = template.createBox();
                     cityContainer.appendChild(newBox);
                 });
